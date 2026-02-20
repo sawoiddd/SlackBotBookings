fe982a47a717d8e78ec48b1441e7c74b66f00f81
oleksandrsafandulaKSE <oleksandr.safandula.25@kse.org.ua>
2026-02-20 16:52:05 +0200

    changed basic configaration tp async one

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-import slack_bolt
-from configJsonReader import ConfigJsonReader
-from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_bolt.async_app import AsyncApp
 
+from configJsonReader import ConfigJsonReader
+import asyncio
+from slack_bolt.adapter.socket_mode.aiohttp import AsyncSocketModeHandler
 
 config_path = "config.json"
 
 tokens = ConfigJsonReader.GetTokens(config_path)
 
 #bot token
-app = slack_bolt.App(token=tokens["bot-token"])
+app = AsyncApp(token=tokens["bot-token"])
 
-#app token
-SocketModeHandler(app, tokens["app-token"]).start()
+async def main():
+    handler = AsyncSocketModeHandler(app, tokens["app-token"])
+    await handler.start_async()
+
+
+if __name__ == "__main__":
+    # Start the async event loop
+    asyncio.run(main())
